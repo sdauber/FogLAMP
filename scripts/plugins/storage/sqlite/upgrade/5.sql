73ebed9a7d53f9c5420c6260cb6adcc26c1520f8
Massimiliano Pinto <massimiliano.pinto@gmail.com>
2018-09-18 10:00:18 +0200

    FOGL-1844: fixed default values in UPDATE
    FOGL-1844: fixed default values in UPDATE

--- scripts/plugins/storage/sqlite/upgrade/5.sql
+++ scripts/plugins/storage/sqlite/upgrade/5.sql
@@ -1,17 +1,16 @@
 UPDATE foglamp.configuration SET value = json_set(value, '$.stream_id', json('{"type":"integer","description":"Stream ID","default":"1","value":"1"}'))
        WHERE key = 'North Readings to PI';
-
-UPDATE foglamp.configuration SET value = json_set(value, '$.source', json('{"description": "Source of data to be sent on the stream. May be either readings, statistics or audit.", "type": "string", "default": "audit", "value": "readings"}'))
+UPDATE foglamp.configuration SET value = json_set(value, '$.source', json('{"description": "Source of data to be sent on the stream. May be either readings, statistics or audit.", "type": "string", "default": "readings", "value": "readings"}'))
         WHERE key = 'North Readings to PI';
 
 UPDATE foglamp.configuration SET value = json_set(value, '$.stream_id', json('{"type":"integer","description":"Stream ID","default":"2","value":"2"}'))
         WHERE key = 'North Statistics to PI';
-UPDATE foglamp.configuration SET value = json_set(value, '$.source', json('{"description": "Source of data to be sent on the stream. May be either readings, statistics or audit.", "type": "string", "default": "audit", "value": "statistics"}'))
+UPDATE foglamp.configuration SET value = json_set(value, '$.source', json('{"description": "Source of data to be sent on the stream. May be either readings, statistics or audit.", "type": "string", "default": "statistics", "value": "statistics"}'))
         WHERE key = 'North Statistics to PI';
 
 UPDATE foglamp.configuration SET value = json_set(value, '$.stream_id', json('{"type":"integer","description":"Stream ID","default":"4","value":"4"}'))
         WHERE key = 'North Readings to OCS';
-UPDATE foglamp.configuration SET value = json_set(value, '$.source', json('{"description": "Source of data to be sent on the stream. May be either readings, statistics or audit.", "type": "string", "default": "audit", "value": "readings"}'))
+UPDATE foglamp.configuration SET value = json_set(value, '$.source', json('{"description": "Source of data to be sent on the stream. May be either readings, statistics or audit.", "type": "string", "default": "readings", "value": "readings"}'))
         WHERE key = 'North Readings to OCS';
 
 UPDATE statistics SET key = 'North Readings to PI' WHERE key = 'SENT_1';
